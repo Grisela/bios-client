c045c2a829196b1f92599b44f4f4f6b220a78a3e
Grisela <griseladiki@gmail.com>
2022-07-17 16:08:14 +0700

    configuring firebase

---
+++ config/firebase/firebase.ts
@@ -0,0 +1,51 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_authDomain,
+  projectId: process.env.NEXT_PUBLIC_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_appId,
+  measurementId: process.env.NEXT_PUBLIC_measurementId,
+};
+
+const initFirebase = () => {
+  if (typeof window !== undefined) {
+    initializeApp(firebaseConfig);
+    console.log("Firebase has been init successfully");
+  }
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db: any = getFirestore(app);
+
+export {
+  initFirebase,
+  db,
+  getStorage,
+  ref,
+  getDownloadURL,
+  getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  updateProfile,
+};
